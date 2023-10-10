63099533f926b94f80866a6c54aff06e82f007b6
Андрей Прокофьев <65781959+kensoi@users.noreply.github.com>
2023-10-11 03:38:46 +0500

    About page

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -1,34 +1,14 @@
-import {
-    CardBlock
-} from "@webx/forms"
-
-import LinkButton from "../Repos/components/LinkButton"
+import "./stylesheet.scss"
+import Illustration from "./Illustration.png"
 
 function Main () {
-    return <div className="repos-wrapper">
-        <h1>
-            Главная
-        </h1>
-        <CardBlock className="repos-page options">
+    return <div className="about-wrapper">
+        <div>
+            <img src={Illustration} />
             <p>
-                Здесь я не придумал чё написать {":)"}
+                Тшшш...
             </p>
-            <p>
-                Могу написать что делаю какие-то штуки, которыми мог бы пользоваться я сам (и я этим пользуюсь), и которыми могли бы пользоваться люди из моего окружения.
-            </p>
-            <p>
-                Работаю с ReactJS, Python
-            </p>
-        </CardBlock>
-        <h6>
-            Полезные ссылки
-        </h6>
-        <CardBlock className="repos-page options">
-            <p>
-                <LinkButton title="Github" link="https://github.com/kensoi" />
-                <LinkButton title="VK" link="https://vk.com/kensoi" />
-            </p>
-        </CardBlock>
+        </div>
     </div>
 }
 
