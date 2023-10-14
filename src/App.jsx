2d71acd0f6419ed6b0e028666d7645b11cf1bd7e
Андрей Прокофьев <65781959+kensoi@users.noreply.github.com>
2023-10-14 21:16:37 +0500

    TT Travels font upload

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import {
 } from "@webx/toolkit"
 
 import Header from "./Header"
+import Router from "./Pages"
 
 import "./scss/stylesheet.scss"
 
 function App () {
     return <ToolKitContext>
         <Header />
+        <Router />
     </ToolKitContext>
 }
 
