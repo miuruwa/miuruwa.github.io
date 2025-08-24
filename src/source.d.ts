468f6e43fe9eec38a13e0b869b8be994d29354ba
Andrey Prokofiev <miuruwadev@gmail.com>
2025-08-24 05:50:55 +0500

    fix [TS]: fixed unused imports

--- src/source.d.ts
+++ src/source.d.ts
@@ -1,154 +1,161 @@
+declare module '*.module.css' {
+  const classes: { readonly [key: string]: string }
+  export default classes
+}
+
+declare module '*.module.sass' {
+  const classes: { readonly [key: string]: string }
+  export default classes
+}
+
 declare module '*.module.scss' {
-  const classNames: Record<string, string>;
-    export default classNames;
-  }
+  const classes: { readonly [key: string]: string }
+  export default classes
+}
 
 declare module "*.png";
 
 declare module "*.gif";
 
-import type { JSX } from "react";
 
-declare global {
+declare namespace React {
+  type ButtonElement = DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>;
 
-  namespace React {
-    type ButtonElement = DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>;
+  type DivElement = DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement>;
 
-    type DivElement = DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement>;
+  type LinkElement = DetailedHTMLProps<LinkHTMLAttributes<HTMLAnchorElement>, HTMLAnchorElement>;
 
-    type LinkElement = DetailedHTMLProps<LinkHTMLAttributes<HTMLAnchorElement>, HTMLAnchorElement>;
+  type InputElement = DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>;
 
-    type InputElement = DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>;
+  type SelectElement = DetailedHTMLProps<InputHTMLAttributes<HTMLSelectElement>, HTMLSelectElement>;
 
-    type SelectElement = DetailedHTMLProps<InputHTMLAttributes<HTMLSelectElement>, HTMLSelectElement>;
+  type TextareaElement = DetailedHTMLProps<TextareaHTMLAttributes<HTMLTextAreaElement>, HTMLTextAreaElement>;
 
-    type TextareaElement = DetailedHTMLProps<TextareaHTMLAttributes<HTMLTextAreaElement>, HTMLTextAreaElement>;
+  type ImageElement = DetailedHTMLProps<ImgHTMLAttributes<HTMLImageElement>, HTMLImageElement>;
 
-    type ImageElement = DetailedHTMLProps<ImgHTMLAttributes<HTMLImageElement>, HTMLImageElement>;
+  type Setter<T> = Dispatch<SetStateAction<T>>;
 
-    type Setter<T> = Dispatch<SetStateAction<T>>;
+  type Optional<T, K extends keyof T> = Omit<T, K> & Partial<T>;
+}
 
-    type Optional<T, K extends keyof T> = Omit<T, K> & Partial<T>;
+declare namespace Pages {
+  type About = {
+    headline: string,
+    description: string,
+    commissions: {
+      headline: string,
+      note: string,
+      links: Shared.Link[]
+    }
+    hardware: Shared.TagList;
+    software: Shared.TagList;
   }
 
-  namespace Pages {
-    type About = {
+  type Details = {
+    headline: string,
+    preferences: Shared.TagList[],
+    addons: Shared.TagList,
+    turnarounds: {
       headline: string,
       description: string,
-      commissions: {
-        headline: string,
-        note: string,
-        links: Shared.Link[]
-      }
-      hardware: Shared.TagList;
-      software: Shared.TagList;
-    }
+    },
+    links: Shared.Link[],
+  }
 
-    type Details = {
+  type Home = {
+    headline: string,
+    description: string,
+  }
+
+  type NotFound = {
+    headline: string,
+    description: string,
+    button: Shared.Link
+  }
+
+  type Request = {
+    headline: string,
+    characterCount: string,
+    addons: Shared.IdList<Requests.Addons, string>,
+    config: {
+      progress: Shared.IdList<Requests.Progress, string>,
+      color: Shared.IdList<Requests.Color, string>,
+      artwork: Shared.IdList<Requests.Artwork, string>,
+    },
+    links: Shared.Link[],
+  }
+
+  type Settings = {
+    headline: string,
+  }
+
+  type StartUp = {
+    headline: string,
+  }
+
+  type TOS = {
+    headline: string,
+    note: {
       headline: string,
-      preferences: Shared.TagList[],
-      addons: Shared.TagList,
-      turnarounds: {
-        headline: string,
-        description: string,
-      },
+      description: string
+    },
+    terms: {
+      headline: string,
+      list: Shared.Paragraph[]
+    }[],
+    navigate: Shared.Link[]
+  }
+}
+
+declare namespace Translations {
+  type Languages = "eng";
+
+  type Translate<Page> = Record<Languages, Page>
+}
+
+declare namespace Requests {
+  type Addons = "addBackground" | "specialRequest" | "commercialUseFee" | "rushFee";
+  type Progress = "sk" | "ln";
+  type Color = "nn"| "bw"| "sm"| "rn";
+  type Artwork = "bu" | "hb" | "fb" | "ch" | "rs" | "eb";
+  type Config = Shared.IdList<Requests.Progress | Requests.Color | Requests.Artwork, string>
+}
+
+declare namespace Shared {
+  type Footer = {
+    info: {
+      headline: string,
+      description: string
+    }
+    navigation: {
+      headline: string,
       links: Shared.Link[],
+      social: Shared.Link[]
     }
+  };
+  type Currencies = "usd";
 
-    type Home = {
-      headline: string,
-      description: string,
-    }
+  type CurrencyList = Record<Currencies, number>;
 
-    type NotFound = {
-      headline: string,
-      description: string,
-      button: Shared.Link
-    }
-
-    type Request = {
-      headline: string,
-      characterCount: string,
-      addons: Shared.IdList<Requests.Addons, string>,
-      config: {
-        progress: Shared.IdList<Requests.Progress, string>,
-        color: Shared.IdList<Requests.Color, string>,
-        artwork: Shared.IdList<Requests.Artwork, string>,
-      },
-      links: Shared.Link[],
-    }
-
-    type Settings = {
-      headline: string,
-    }
-
-    type StartUp = {
-      headline: string,
-    }
-
-    type TOS = {
-      headline: string,
-      note: {
-        headline: string,
-        description: string
-      },
-      terms: {
-        headline: string,
-        list: Shared.Paragraph[]
-      }[],
-      navigate: Shared.Link[]
-    }
-  }
-  namespace Translations {
-    type Languages = "eng";
-
-    type Translate<Page> = Record<Languages, Page>
+  type TagList = {
+    headline: string,
+    list: string[],
   }
 
-  namespace Requests {
-    type Addons = "addBackground" | "specialRequest" | "commercialUseFee" | "rushFee";
-    type Progress = "sk" | "ln";
-    type Color = "nn"| "bw"| "sm"| "rn";
-    type Artwork = "bu" | "hb" | "fb" | "ch" | "rs" | "eb";
-    type Config = Shared.IdList<Requests.Progress | Requests.Color | Requests.Artwork, string>
+  type IdList<Ids, Value> = {
+    headline: string,
+    list: Record<Ids, Value>,
   }
 
-  namespace Shared {
-    type Footer = {
-      info: {
-        headline: string,
-        description: string
-      }
-      navigation: {
-        headline: string,
-        links: Shared.Link[],
-        social: Shared.Link[]
-      }
-    };
-    type Currencies = "usd";
-
-    type CurrencyList = Record<Currencies, number>;
-
-    type TagList = {
-      headline: string,
-      list: string[],
-    }
-
-    type IdList<Ids, Value> = {
-      headline: string,
-      list: Record<Ids, Value>,
-    }
-
-    type Link = {
-      icon?: JSX.Element,
-      title?: string,
-      path?: string,
-      location?: string,
-    }
-
-    type Paragraph = (string | {
-      headline: string,
-      list: string[]
-    })
+  type Link = {
+    icon?: import("react").JSX.Element,
+    title?: string,
+    path?: string,
+    location?: string,
   }
+
+  type Paragraph = (string | {
+    headline: string,
+    list: string[]
+  })
 }
